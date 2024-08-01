c7746f4cd8d63697aa0950fb1bd76912cf3b6fc4
Kike <kike.mendoza9978@gmail.com>
2024-07-31 19:29:16 -0600

    Fix 404 serving logic

--- index.js
+++ index.js
@@ -16,18 +16,13 @@ const server = http.createServer((req, res) => {
                 res.write(data)
                 return res.end(`${filepath} was not found`)
             })
-
-        }
-        if (filepath === "/404") {
             return
         }
+
         res.writeHead(200, { "Content-Type": "text/html" })
         return res.end(data)
     })
-
-
 })
-
 
 const port = 8000
 server.listen(8000, () => {
